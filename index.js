cfc6b25499cf6b09ed3e640503441c9a72fa9b72
IBUNHABIBU <ibunhabib@gmail.com>
2023-10-19 21:11:11 +0300

    Add config variables

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ var server = http.createServer(function(req, res) {
 })
 
 server.listen(config.port, function() {
-    console.log("The server is listening on port "+ config.port +" now");
+    console.log("The server is listening on port "+ config.port +" in " + config.envName +"  now");
 })
 
 // define the handlers
