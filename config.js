c550c1d1db224db7baabea45ae08d0420a540d12
IBUNHABIBU <ibunhabib@gmail.com>
2023-10-19 13:49:31 +0300

    Add config variables

--- config.js
+++ config.js
@@ -4,3 +4,10 @@ create and export configuration variables
 
 // Container for all the environments
 var environments = {};
+
+// Staging (default) environment
+environments.staging = {
+    'port' : 3000,
+    'envName' : 'staging'
+
+};
