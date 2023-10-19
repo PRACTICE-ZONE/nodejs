7a8f176e72ce0bfcf31826647f70606e09c3b5d1
IBUNHABIBU <ibunhabib@gmail.com>
2023-10-19 21:15:02 +0300

    Finish config file

--- config.js
+++ config.js
@@ -24,5 +24,6 @@ var currentEnvironment = typeof(process.env.NODE_ENV) == 'string'? process.env.N
 // Check that the current environment is one of the environments above, if not, default to staging
 var environmentToExport = typeof(environments[currentEnvironment]) == 'object'? environments[currentEnvironment] : environments.staging;
 
+
 // Export the module
 module.exports = environmentToExport;
